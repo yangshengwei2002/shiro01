d18812c3f415e6a5931d93e3dc79c10a34859226
yangshengwei2002 <3288571913@qq.com>
2021-12-29 19:06:10 +0800

    Initial commit

---
+++ src/main/java/com/zking/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.zking.controller;
+
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "login";
+    }
+
+
+    @RequiresPermissions("用户管理")
+    @RequestMapping("/update")
+    public String updateUser(){
+        return "admin/updateUser";
+    }
+}
